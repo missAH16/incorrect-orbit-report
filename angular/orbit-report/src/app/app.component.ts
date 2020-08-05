58119100bb5377b5db5839abc0cb76637c654d8c
missAH16 <missahiatt@gmail.com>
2020-08-05 17:56:44 -0500

    3

--- angular/orbit-report/src/app/app.component.ts
+++ angular/orbit-report/src/app/app.component.ts
@@ -14,13 +14,13 @@ export class AppComponent {
   constructor() {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
-    
-   window.fetch(satellitesUrl).then(function(response) {
-    response.json().then(function(data) {
-
-       let fetchedSatellites = data.satellites;
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
        for (let i = 0; i < fetchedSatellites.length; i++) {
-         let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].orbitType, fetchedSatellites[i].launchDate, fetchedSatellites[i].type, fetchedSatellites[i].operational);
+         let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
         this.sourceList.push(satellite);
         }
     }.bind(this));
