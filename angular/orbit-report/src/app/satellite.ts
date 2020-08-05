cdd4406270762089e5b486820f2988604f11d8b9
missAH16 <missahiatt@gmail.com>
2020-08-05 15:04:57 -0500

    b

--- angular/orbit-report/src/app/satellite.ts
+++ angular/orbit-report/src/app/satellite.ts
@@ -1,14 +1,13 @@
-
-
 export class Satellite {
     name: string;
     type: string;
     launchDate: string;
     orbitType: string;
     operational: boolean;
-    constructor(name:string, type:string, launchDate:string, orbitType:string, operational:true){
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
         this.orbitType = orbitType;
+        this.launchDate = launchDate;
         this.type = type;
         this.operational = operational;
     }
