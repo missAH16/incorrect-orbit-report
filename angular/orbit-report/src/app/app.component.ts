360d5b15532629b23f735b3794f707213305f20b
missAH16 <missahiatt@gmail.com>
2020-08-06 16:12:45 -0500

    submitting

--- angular/orbit-report/src/app/app.component.ts
+++ angular/orbit-report/src/app/app.component.ts
@@ -15,11 +15,11 @@ export class AppComponent {
     this.sourceList = [];
     this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
- console.log(satellitesUrl)
+   
     window.fetch(satellitesUrl).then(function(response) {
        response.json().then(function(data) {
  
-          let fetchedSatellites = data.satellites;
+      let fetchedSatellites = data.satellites;
        for (let i = 0; i < fetchedSatellites.length; i++) {
          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
         this.sourceList.push(satellite);
@@ -28,4 +28,19 @@ export class AppComponent {
     }.bind(this));
  }.bind(this));
 }
-  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       let orbitType = this.sourceList[i].orbitType.toLowerCase();
+       let type = this.sourceList[i].type.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+     }
+     this.displayList = matchingSatellites;
+}
+}
+    
